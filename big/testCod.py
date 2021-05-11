514fa96f7528b3bacec5a19fb470d560e11033be
Sebastian Savu <sebi.savu@finalsite.com>
2021-05-11 11:28:35 +0300

    a

--- big/testCod.py
+++ big/testCod.py
@@ -179,10 +179,10 @@ if __name__ == '__main__':
 
 	# Convert images & masks to arrays
 	for image in images:
-		img_orig = np.array(Image.open(f'aerial_image_dataset/training/images/{image}').resize((256, 256, 3)))
-		x_img = np.reshape(img_orig, (256, 256, 3))
-		mask_orig = np.array(Image.open(f'aerial_image_dataset/training/gt/{image}').resize((256, 256, 3)))
-		mask = np.reshape(mask_orig, (256, 256, 3))
+		x_img = np.array(Image.open(f'aerial_image_dataset/training/images/{image}').resize((256, 256, 3)))
+		# x_img = np.reshape(img_orig, (256, 256, 3))
+		mask = np.array(Image.open(f'aerial_image_dataset/training/gt/{image}').resize((256, 256, 3)))
+		# mask = np.reshape(mask_orig, (256, 256, 3))
 
 		X[index] = x_img / 255.0
 		y[index] = mask / 255.0
