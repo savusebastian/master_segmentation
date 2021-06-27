59db5ebb2aaabce18927c57e354f2309670db47f
Savu Sebastian <savusebastianf@gmail.com>
2021-06-27 11:55:08 +0300

    a

--- big/model.py
+++ big/model.py
@@ -183,22 +183,22 @@ def get_efficientnet_unet(input_shape):
 	# conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
 	# drop5 = Dropout(0.5)(conv5)
 
-	up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(bl9))
+	up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(bl9)
 	merge6 = Concatenate(axis=3) ([bl4, up6])
 	conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
 	conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
-	up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+	up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 	merge7 = Concatenate(axis=3) ([bl3, up7])
 	conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
 	conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
-	up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+	up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 	merge8 = Concatenate(axis=3) ([bl2, up8])
 	conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
 	conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
-	up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+	up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 	merge9 = Concatenate(axis=3) ([bl1, up9])
 	conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
 	conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
