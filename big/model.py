fee4ddf74fca6819bd6904566645c62fc0bb107c
Savu Sebastian <savusebastianf@gmail.com>
2021-06-27 13:17:47 +0300

    a

--- big/model.py
+++ big/model.py
@@ -194,6 +194,7 @@ def get_efficientnet_unet(input_shape):
 	ebl8 = Concatenate(axis=3) ([bl8, ebl8])
 	ebl8 = Conv2D(320, 3, activation='relu', padding='same', kernel_initializer='he_normal')(ebl8)
 	ebl8 = Conv2D(320, 3, activation='relu', padding='same', kernel_initializer='he_normal')(ebl8)
+	pool8 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(ebl8)
 
 	ebl7 = Conv2D(192, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(ebl8))
 	ebl7 = Concatenate(axis=3) ([bl7, ebl7])
