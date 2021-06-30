ae3918536a41c1b8f3709af96a0d52abb17f04a2
Savu Sebastian <savusebastianf@gmail.com>
2021-06-30 17:39:59 +0300

    a

--- big/model.py
+++ big/model.py
@@ -123,11 +123,11 @@ def get_efficientnet_unet(input_shape):
 	def s_e(input_shape, filters, r=24):
 		# For other output sizes in Keras, you need to use AveragePooling2D, but you can't specify the output shape directly. You need to calculate/define the pool_size, stride, and padding parameters depending on how you want the output shape. If you need help with the calculations, check this page of CS231n course.
 		# https://cs231n.github.io/convolutional-networks/#pool
-		o = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(input_shape)
-		print(filters, r)
 		if filters < r * 2:
 			r = filters
-		print(filters, r)
+
+		o = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(input_shape)
+
 		# ap2d = tf.keras.layers.AveragePooling2D()(input_shape)
 		ap2d = tf.nn.avg_pool2d(input_shape, 2, 1, 'SAME')
 		c2d1 = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(ap2d)
@@ -268,10 +268,10 @@ def get_efficientnet_as_unet(input_shape):
 	def s_e(input_shape, filters, r=24):
 		# For other output sizes in Keras, you need to use AveragePooling2D, but you can't specify the output shape directly. You need to calculate/define the pool_size, stride, and padding parameters depending on how you want the output shape. If you need help with the calculations, check this page of CS231n course.
 		# https://cs231n.github.io/convolutional-networks/#pool
-		o = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(input_shape)
-
 		if filters < r * 2:
 			r = filters
+
+		o = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(input_shape)
 
 		mp2d = tf.keras.layers.MaxPooling2D()(input_shape)
 		c2d1 = tf.keras.layers.Conv2D(filters // r, kernel_size=1)(mp2d)
