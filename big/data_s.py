ae5283c60d9ac10046651a4eead5fbdeac367c02
Savu Sebastian <savusebastianf@gmail.com>
2021-07-04 16:55:49 +0300

    a

--- big/data_s.py
+++ big/data_s.py
@@ -150,3 +150,26 @@ if __name__ == '__main__':
 	# 	validation_data=dataset['val'],
 	# 	epochs=epochs
 	# )
+
+
+	# Plot Loss vs Epoch
+	plt.figure()
+	plt.title('Learning curve')
+	plt.plot(results.history['loss'], label='loss')
+	plt.plot(results.history['val_loss'], label='val_loss')
+	plt.plot(np.argmin(results.history['val_loss']), np.min(results.history['val_loss']), marker='x', label='best model')
+	plt.xlabel('Epochs')
+	plt.ylabel('log_loss')
+	plt.legend()
+	plt.show()
+
+	# Plot Accuracy vs Epoch
+	acc = results.history['acc']
+	val_acc = results.history['val_acc']
+	epochs = range(len(acc))
+	plt.figure()
+	plt.title('Training and validation accuracy')
+	plt.plot(epochs, acc, label='Training acc')
+	plt.plot(epochs, val_acc, label='Validation acc')
+	plt.legend()
+	plt.show()
